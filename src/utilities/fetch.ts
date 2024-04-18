e4511a9de448f9b5cae290f76a9fd223898d2384
axunion <120321953+axunion@users.noreply.github.com>
2024-04-19 01:17:54 +0900

    Refine fetch

--- src/utilities/fetch.ts
+++ src/utilities/fetch.ts
@@ -1,6 +1,11 @@
 export class FetchError extends Error {
   constructor(resource: string, status: number, statusText: string) {
-    super(`Failed to fetch data from ${resource}: ${status} ${statusText}`);
+    super(
+      `Failed to fetch data from ${resource}${
+        status ? `: ${status} ${statusText}` : ""
+      }`,
+    );
+
     this.name = "FetchError";
     Object.setPrototypeOf(this, new.target.prototype);
   }
@@ -10,43 +15,24 @@ export const fetchJson = async <T>(
   resource: string,
   init?: RequestInit,
 ): Promise<T> => {
-  try {
-    const response = await fetch(resource, init);
+  const response = await fetch(resource, init);
 
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new FetchError(resource, response.status, response.statusText);
-    }
-  } catch (error) {
-    if (error instanceof Error) {
-      throw error;
-    } else {
-      throw new Error(`An unexpected error occurred: ${String(error)}`);
-    }
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new FetchError(resource, response.status, response.statusText);
   }
 };
 
 export const fetchBlob = async (
   resource: string,
   init?: RequestInit,
-): Promise<Uint8Array> => {
-  try {
-    const response = await fetch(resource, init);
+): Promise<Blob> => {
+  const response = await fetch(resource, init);
 
-    if (response.ok) {
-      const blob = await response.blob();
-      const buffer = await blob.arrayBuffer();
-
-      return new Uint8Array(buffer);
-    } else {
-      throw new FetchError(resource, response.status, response.statusText);
-    }
-  } catch (error) {
-    if (error instanceof Error) {
-      throw error;
-    } else {
-      throw new Error(`An unexpected error occurred: ${String(error)}`);
-    }
+  if (response.ok) {
+    return await response.blob();
+  } else {
+    throw new FetchError(resource, response.status, response.statusText);
   }
 };
